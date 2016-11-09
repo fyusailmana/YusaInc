1b271af6a155972549b36dfacfe41ce52ba73e1a
Fahmi Yusa Ilmana <fhmi_yusa@yahoo.co.id>
2016-11-09 03:18:44 -0600

    Add files via upload

--- Web/Script/loading.js
+++ Web/Script/loading.js
@@ -1,21 +1,3 @@
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
-}
-
 (function(){
   function id(v){return document.getElementById(v); }
   function loadbar() {
@@ -48,15 +30,3 @@ function showDivs(n) {
   }
   document.addEventListener('DOMContentLoaded', loadbar, false);
 }());
-
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-    document.body.style.backgroundColor = "white";
-}
